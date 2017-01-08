d0e3678054d247f0a966c910e37fb4c582e9c2a2
Anastasiya <ovchinnickova.anast@gmail.com>
2017-01-08 19:57:55 +0500

    Update models.py

--- pyramid_pizza/models.py
+++ pyramid_pizza/models.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 from pyramid_sqlalchemy import BaseObject
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from sqlalchemy.orm import backref, relationship
 
 engine = create_engine('sqlite:///base.db')
 Session = sessionmaker(bind=engine,extension=ZopeTransactionExtension()))
@@ -35,3 +38,30 @@ class Order(BaseObject):
     id_User = Column(Unicode(255), ForeignKey('User.id_User'))
     price_Order = Column(Integer)
     sale = Column(Integer)
+
+def sacrud_settings(config):
+    config.include('pyramid_sacrud', route_prefix='admin')
+    config.registry.settings['pyramid_sacrud.models'] = (
+        ('zakaz', [Order, Tovar]),
+        ('group', [User])
+    )
+
+
+def database_settings(config):
+    from sqlalchemy import create_engine
+    config.registry.settings['sqlalchemy.url'] = db_url =\
+        "sqlite:///base.db"
+    engine = create_engine(db_url)
+    Base.metadata.bind = engine
+    Base.metadata.create_all()
+
+
+if __name__ == '__main__':
+    from pyramid.session import SignedCookieSessionFactory
+    my_session_factory = SignedCookieSessionFactory('123')
+    config = Configurator(session_factory=my_session_factory)
+    config.include(database_settings)
+    config.include(sacrud_settings)
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
