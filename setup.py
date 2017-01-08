1858cef32a52cb484b4486dff74ffefddbb8da26
Anastasiya <ovchinnickova.anast@gmail.com>
2017-01-08 19:24:39 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+
+from setuptools import setup, find_packages
+
+Area = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(Area, 'README.txt')) as f:
+    README = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy']
+
+setup(name='pyramid_pizza',
+      version='0.0',
+      description='pyramid_pizza',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_pizza',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_blogr:main
+      [console_scripts]
+      initialize_pyramid_pizza_db = pyramid_pizza.scripts.initdb:main
+      """,
+      )
