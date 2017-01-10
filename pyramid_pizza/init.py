f580688b4e358231d0f1178393021e2528be2a14
Anastasiya <ovchinnickova.anast@gmail.com>
2017-01-10 12:05:47 +0500

    Update init.py

--- pyramid_pizza/init.py
+++ pyramid_pizza/init.py
@@ -4,6 +4,13 @@ from pyramid.response import Response
 from .models import User, Tovar, Order, Session, Base, engine
 import transaction
 
+def database_settings(config):
+    from sqlalchemy import create_engine
+    config.registry.settings['sqlalchemy.url'] = db_url = "sqlite:///example.db"
+    engine = create_engine(db_url)
+    Base.metadata.bind = engine
+    Base.metadata.create_all()
+    
 def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     Session.configure(bind=engine)
@@ -12,3 +19,10 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     
     return config.make_wsgi_app()
+
+if __name__ == '__main__':
+    config = Configurator()
+    config.include(database_settings)
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6543, app)
+    server.serve_forever()
